df1ef4726c3cc120d3b5135b70f95bcb701d8b49
Jimmy Lam <jimmy.lam@easycode-it.com>
2023-01-31 08:54:53 +0100

    Edited card model

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -2,17 +2,29 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.UUID;
+
 
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import model.Card;
 import model.CardsStack;
 
 public class Controller implements Initializable {
+    @FXML
+    private TextField germanTxtField;
+
+    @FXML
+    private TextField foreignTxtField;
+    
+    UUID uuid = UUID.randomUUID();
+    String uuidAsString = uuid.toString();
     private CardsStack englishStack = new CardsStack();
-    private String[] languages= { "Deutsch", "Englisch", "Französich", "Italienisch" };
+    private String[] languages= {"Englisch", "Französich", "Italienisch" };
 
     @FXML
     private ChoiceBox<String> choiceBoxLanguage;
@@ -33,18 +45,35 @@ public class Controller implements Initializable {
     private void initialize() {
 
         System.out.println("Initialize");
-              // Füge Optionen hinzu und setze eine Standardauswahl
-    //   choiceBox.getItems().add("Option 1");
-    //   choiceBox.getItems().add("Option 2");
-    //   choiceBox.getItems().add("Option 3");
-    //   choiceBox.setValue("Option 1");
-
-    //   // Füge einen ChangeListener hinzu
-    //   choiceBox.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-    //      System.out.println("Aktuelles Element: " + newValue);
-    //   });
    
     }
 
+    @FXML
+    public void addNewCard() {
+        System.out.println("Click hinzufügen");
+        Card newCard = new Card();
 
+        newCard.setWord(germanTxtField.getText());
+        
+    }
 }
+
+// Task newTask = new Task();
+// newTask.setTitel(titleTxt.getText());
+// newTask.setDescription(descriptionTxt.getText());
+// newTask.setDueDate(DateUtil.parse(dueDayTxt.getText()));
+// if(this.priorityGroup.getSelectedToggle().equals(this.rbHigh)) {
+//     newTask.setPriority("1 Hoch");
+// } else if (this.priorityGroup.getSelectedToggle().equals(this.rbMiddle)) {
+//     newTask.setPriority("2 Mittel");
+// } else {
+//     newTask.setPriority("3 Niedrig");
+// }
+// showTaskDetails(newTask);
+// tabPane.getSelectionModel().select(tabOverview);
+// systemLabel.setText("Aufgabe gespeichert.");
+// Timeline timeline = new Timeline(new KeyFrame(
+// Duration.millis(6000),
+// ae -> systemLabel.setText("")));
+// timeline.play();
+// taskTable.getItems().add(newTask);
--- src/main/java/model/Card.java
+++ src/main/java/model/Card.java
@@ -1,13 +1,16 @@
 package model;
 
 import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+
 
 import interfaces.Deck;
 
 public class Card implements Deck {
 
-    private StringProperty id;
+    private String id;
     private StringProperty word;
 	private StringProperty foreignWord;
 	private BooleanProperty learned;
@@ -15,43 +18,58 @@ public class Card implements Deck {
     public Card() {
     }
 
-    public Card(StringProperty id, StringProperty word, StringProperty foreignWord, BooleanProperty learned) {
+    public Card(String id, String word, String foreignWord, Boolean learned) {
         this.id = id;
-        this.word = word;
-        this.foreignWord = foreignWord;
-        this.learned = learned;
+        this.word = new SimpleStringProperty(word);
+        this.foreignWord = new SimpleStringProperty(foreignWord);
+        this.learned = new SimpleBooleanProperty(learned);
     }
 
-    public StringProperty getId() {
+    public String getId() {
         return this.id;
     }
 
-    public void setId(StringProperty id) {
+    public void setId(String id) {
         this.id = id;
     }
 
-    public StringProperty getWord() {
-        return this.word;
+    //Word
+    public String getWord() {
+        return word.get();
     }
 
-    public void setWord(StringProperty word) {
-        this.word = word;
+    public void setWord(String word) {
+        this.word.set(word);
     }
 
-    public StringProperty getForeignWord() {
-        return this.foreignWord;
+	public StringProperty wordProperty() {
+		return word;
+	}
+
+    //Foreign word
+    public String getForeignWord() {
+        return foreignWord.get();
     }
 
-    public void setForeignWord(StringProperty foreignWord) {
-        this.foreignWord = foreignWord;
+    public void setForeignWord(String foreignWord) {
+        this.foreignWord.set(foreignWord);
     }
 
-    public BooleanProperty getLearned() {
-        return this.learned;
+    public StringProperty foreignWordProperty() {
+		return foreignWord;
+	}
+
+    //Gelernd
+    public boolean getLearned() {
+        return learned.get();
     }
 
-    public void setLearned(BooleanProperty learned) {
-        this.learned = learned;
+    public void setLearned(boolean learned) {
+        this.learned.set(learned);
+    }
+
+    public BooleanProperty learnedProperty() {
+        return learned;
     }
 
     @Override
